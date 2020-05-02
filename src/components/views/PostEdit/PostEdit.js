6c24f4dd4e5cd1c6164e531a8d6f8fccaa0d233d
Klaudia Skóra <klaud.skora@gmail.com>
2020-05-02 23:01:12 +0200

    Create action update_post

--- src/components/views/PostEdit/PostEdit.js
+++ src/components/views/PostEdit/PostEdit.js
@@ -5,56 +5,72 @@ import clsx from 'clsx';
 
 import { NotFound } from '../NotFound/NotFound';
 import { connect } from 'react-redux';
-// import { reduxSelector, reduxActionCreator } from '../../../redux/exampleRedux.js';
+import { updatePost } from '../../../redux/postsRedux.js';
 
 import styles from './PostEdit.module.scss';
 
-const Component = ({ post, className }) => {
+class Component extends React.Component {
 
-  if(!post) {
-    return <NotFound />;
+  static propTypes = {
+    className: PropTypes.string,
+    post: PropTypes.object,
+    fetchData: PropTypes.func,
+  };
+
+  state = {
+    title: 'new title',
+    text: 'new text',
+    email: 'new email',
   }
 
-  const {title, text, email} = post;
+  submitChanges() {
+    const { fetchData } = this.props;
 
-  return (
-    <div className={clsx(className, styles.root)}>
-      <form className={styles.post}>
-        <div className={styles.dataBox}>
-          <h2 className={styles.title}>Title</h2>
-          <input className={styles.window} type="text" defaultValue={title} minLength="10"></input>
-        </div>
-        <div className={styles.dataBox}>
-          <h2 className={styles.title}>Post content</h2>
-          <textarea className={styles.window} type="text" defaultValue={text} minLength="20"></textarea>
-        </div>
-        <div className={styles.dataBox}>
-          <h2 className={styles.title}>Author&apos;s email</h2>
-          <input className={styles.window} type="email" defaultValue={email}></input>
-        </div>
-        <button className={styles.submit} type="submit">Submit changes</button>
-      </form>
-    </div>
-  );
-};
+    fetchData( this.state.title, this.state.text, this.state.email);
 
-Component.propTypes = {
-  children: PropTypes.node,
-  className: PropTypes.string,
-  match: PropTypes.object,
-  post: PropTypes.object,
-  logged: PropTypes.bool,
-};
+  }
+
+  render() {
+    const { className, post } = this.props;
+
+    if(!post) {
+      return <NotFound />;
+    }
+
+    const { title, text, email } = post;
+
+    return(
+      <div className={clsx(className, styles.root)}>
+        <form className={styles.post}>
+          <div className={styles.dataBox}>
+            <h2 className={styles.title}>Title</h2>
+            <input className={styles.window} type="text" defaultValue={title} minLength="10"></input>
+          </div>
+          <div className={styles.dataBox}>
+            <h2 className={styles.title}>Post content</h2>
+            <textarea className={styles.window} type="text" defaultValue={text} minLength="20"></textarea>
+          </div>
+          <div className={styles.dataBox}>
+            <h2 className={styles.title}>Author&apos;s email</h2>
+            <input className={styles.window} type="email" defaultValue={email}></input>
+          </div>
+          <button onClick={ (e) => this.submitChanges() }className={styles.submit} type="submit">Submit changes</button>
+        </form>
+      </div>
+    );
+  }
+}
+
 
 const mapStateToProps = (state, props) => ({
   post: state.posts[props.match.params.id],
 });
 
-// const mapDispatchToProps = dispatch => ({
-//   someAction: arg => dispatch(reduxActionCreator(arg)),
-// });
+const mapDispatchToProps = (dispatch, props) => ({
+  fetchData: ( title, text, email) => dispatch(updatePost( props.match.params.id, title, text, email)),
+});
 
-const Container = connect(mapStateToProps)(Component);
+const Container = connect(mapStateToProps, mapDispatchToProps)(Component);
 
 export {
   // Component as PostEdit,
--- src/redux/postsRedux.js
+++ src/redux/postsRedux.js
@@ -9,11 +9,13 @@ const createActionName = name => `app/${reducerName}/${name}`;
 const FETCH_START = createActionName('FETCH_START');
 const FETCH_SUCCESS = createActionName('FETCH_SUCCESS');
 const FETCH_ERROR = createActionName('FETCH_ERROR');
+const UPDATE_POST = createActionName('UPDATE_POST');
 
 /* action creators */
 export const fetchStarted = payload => ({ payload, type: FETCH_START });
 export const fetchSuccess = payload => ({ payload, type: FETCH_SUCCESS });
 export const fetchError = payload => ({ payload, type: FETCH_ERROR });
+export const updatePost = payload => ({ payload, type: UPDATE_POST });
 
 /* thunk creators */
 
@@ -42,6 +44,16 @@ export const reducer = (statePart = [], action = {}) => {
     case FETCH_ERROR: {
       return {
         ...statePart,
+        loading: {
+          active: false,
+          error: action.payload,
+        },
+      };
+    }
+    case UPDATE_POST: {
+      return {
+        ...statePart,
+        data: statePart.data.posts.map(post => post.id === action.id ? {...post, post: action.post } : post ),
         loading: {
           active: false,
           error: action.payload,
